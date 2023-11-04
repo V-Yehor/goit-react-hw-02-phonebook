17b1ff327a53ddc7eef5d7dd246a0fbdb5947852
V-Yehor <130846410+V-Yehor@users.noreply.github.com>
2023-11-04 16:25:10 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import { GlobalStyle } from './GlobalStyle';
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      React homework template
+      <GlobalStyle />
+    </div>
+  );
+};
